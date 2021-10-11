7809fc05a47d29bd41ad6320e55de3731b87d9d7
Divyarth Singh <div8791@gmail.com>
2021-10-11 21:13:37 +0530

    Add files via upload

---
+++ insertionsort.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+void bubs(vector<int> &a, int n)
+{
+    for(int i=1; i<n; i++)
+    {
+        int curr = a[i];
+        int j= i-1;
+        while(a[j]>curr && j>=0)
+        {
+            a[j+1] = a[j];
+            j--;
+        }
+        a[j+1] = curr;
+    }
+}
+signed main()
+{
+    int n; cin>>n;
+    vector<int> a(n);
+    for(auto &i: a)
+    {
+        cin>>i;
+    }
+    bubs(a, n);
+    for(auto &i: a)
+    {
+        cout<<i<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
